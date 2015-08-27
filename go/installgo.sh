57c5ff5a27565ae5de1fdafc3d4c4a7f9fcab687
Roi Martin <jroi.martin@gmail.com>
2015-08-27 15:41:52 +0200

    Update go box to go1.5

--- go/installgo.sh
+++ go/installgo.sh
@@ -10,13 +10,17 @@ git clone https://go.googlesource.com/go ~/go1.4
 cd ~/go1.4 && git checkout go1.4.2
 cd ~/go1.4/src && ./make.bash
 
+cp -R ~/go1.4 ~/go
+cd ~/go && git clean -dfx && git reset --hard && git checkout go1.5
+cd ~/go/src && ./make.bash
+
 mkdir -p ~/gocode
-GOPATH=$HOME/gocode ~/go1.4/bin/go get golang.org/x/tools/... github.com/nsf/gocode
+GOPATH=$HOME/gocode ~/go/bin/go get golang.org/x/tools/... github.com/nsf/gocode
 
 ~/gocode/src/github.com/nsf/gocode/vim/pathogen_update.sh
 
 echo 'export GOPATH=$HOME/gocode
-export PATH=$HOME/go1.4/bin:$GOPATH/bin:$PATH
+export PATH=$HOME/go/bin:$GOPATH/bin:$PATH
 export EDITOR=vim' >> ~/.profile
 
 echo '[user]
