0354a0b3ef6a056f9e5243001e04abf87fe4cfd8
Roi Martin <jroi.martin@gmail.com>
2016-11-08 00:08:34 +0100

    Update Go box

--- go/installgo.sh
+++ go/installgo.sh
@@ -5,13 +5,15 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/jroimartin/dotfiles/master/vimrc
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 
 git clone https://go.googlesource.com/go ~/go1.4
-cd ~/go1.4 && git checkout go1.4.2
+cd ~/go1.4 && git checkout release-branch.go1.4
 cd ~/go1.4/src && ./make.bash
 
 cp -R ~/go1.4 ~/go
-cd ~/go && git clean -dfx && git reset --hard && git checkout go1.5.2
+cd ~/go && git clean -dfx && git reset --hard && git checkout go1.7.3
 cd ~/go/src && ./make.bash
 
 mkdir -p ~/gocode
@@ -23,8 +25,4 @@ echo 'export GOPATH=$HOME/gocode
 export PATH=$HOME/go/bin:$GOPATH/bin:$PATH
 export EDITOR=vim' >> ~/.profile
 
-echo '[user]
-	name = Roi Martin
-	email = jroi.martin@gmail.com
-[push]
-	default = simple' >> ~/.gitconfig
+curl -LSso ~/.gitconfig https://raw.githubusercontent.com/jroimartin/dotfiles/master/gitconfig
